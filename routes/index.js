0f722ee08ba8f69f9ec234ef67031d3ea7623ec0
sebasjare08 <sebasjara@gmail.com>
2020-12-12 17:33:24 -0500

    backend

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// importar express
+const router = require('express').Router();
+// traernos el users de api
+const apiRouterUser = require('./api/users')
+
+// decirle que use el apiRouterUser
+router.use('/auth', apiRouterUser);
+
+
+// esportamos router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,51 @@
-/*en caso de  hacer uso con el directorio controlador se 
-debe importar como se observa en la siguiente linea, con el nombre del archivo js
-que contiene la logica */
-//const controller = require('./controller/nombredelcontrollador.js');
+// instancia de express en mi app
 const express = require('express');
 const db = require('./models');
-const app = express()
+// instancias middleware morgan para mostrarme informacion de peticion a mi servidor
+const morgan = require('morgan');
+
+// instancias del index que hay en routes
+const apiRouter = require('./routes');
+// importar bodyparser
 const bodyParser = require('body-parser');
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// importar manejador cors
+const cors = require('cors');
 
+// llamado a mi aplicacion
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+// manejador de Cors
+// app.use((req,res,next)=>{
+//     res.header("Access-Control-Allow-Origin","*");
+//     res.header("Access-Control-Allow-Header","Origin, X-Requested-With, Content-Type, Accept");
+//     res.header('Access-Control-Allow-Methods: GET, POST, DELETE');
+//     next();
+// });
 
-// API ENDPOINTS
-/*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
-'/api/auth/signin'
-*/
+//  usar el middleware morgan
+app.use(morgan('dev'));
+// user el bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+// usar el cors
+app.use(cors());
+
 app.get('/', function(req, res) {
-    console.log("Estructura base del proyecto backend");
-    res.send("Estructura base del proyecto backend");
+    db.user.findAll().then(users => res.json(users))
 });
-const port = 3000
-app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
-})
+
+
+app.get('/api/users',(req,res)=>{
+    db.user.findAll().then(users => res.json(users))
+});
+
+app.use('/api', apiRouter) ;
+
+// Escoger el puerto 
+app.set('PORT', process.env.PORT || 3000);
+// mostrar un mensaje en la consola que si este el servidor
+app.listen(app.get('PORT'), ()=>{
+    console.log(`Running on http://localhost:${app.get('PORT')}`)
+});
 
 module.exports = app;
